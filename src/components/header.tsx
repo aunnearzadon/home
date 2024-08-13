cb2f9239209293e77c129000cb2512b7929792f5
Aunne Arzadon <nodazra@gmail.com>
2024-08-13 16:58:41 +0100

    Fix sidemenu toggling for smaller screens

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,11 @@
+'use client'
+
+import { useState } from 'react';
+
 export default function Header () {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <header className="absolute inset-x-0 top-0 z-50">
       <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
@@ -7,29 +14,29 @@ export default function Header () {
             <span className="sr-only">Aunne Rouie Arzadon</span>
           </a>
         </div>
-        <div className="flex lg:hidden">
-          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+        <div className={ isMenuOpen ? "hidden" : "lg:hidden" }>
+          <button onClick={toggleMenu} type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
             <span className="sr-only">Open main menu</span>
             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+        <div className={ isMenuOpen ? "lg:flex lg:gap-x-12" : "hidden lg:flex lg:gap-x-12"}>
           <a href="/home" className="text-sm font-semibold leading-6 text-gray-900">Home</a>
           <a href="/home/about-me" className="text-sm font-semibold leading-6 text-gray-900">About Me</a>
           <a href="/home/work-experience" className="text-sm font-semibold leading-6 text-gray-900">Work Experience</a>
           <a href="/home/contact-me" className="text-sm font-semibold leading-6 text-gray-900">Contact Me</a>
         </div>
       </nav>
-      <div className="lg:hidden" role="dialog" aria-modal="true">
+      <div className={ isMenuOpen ? "lg:hidden" : "hidden lg:hidden" }role="dialog" aria-modal="true">
         <div className="fixed inset-0 z-50"></div>
         <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="/home" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
             </a>
-            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
+            <button onClick={toggleMenu} type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
               <span className="sr-only">Close menu</span>
               <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
